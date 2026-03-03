a7a363df1ebf5bd097dfb079e93723a55ae231e3
gupta <vgup3012@7-11.com>
2026-03-03 16:29:19 +0530

    Coded UC1 to Initiate the Palindrome App

---
+++ src/com/palindromeapp/UseCaseOnePalindromeApp.java
@@ -0,0 +1,27 @@
+/**
+ * Use Case 1: Application Entry and Welcome Message
+ * 
+ * This class represents the entry point of the
+ * palindrome Checker Management System
+ * 
+ * No Palindrome logic is implemented yet
+ * 
+ * @author vyapti gupta
+ */
+package com.palindromeapp;
+
+/**
+ * First method executed by JVM when the program starts 
+ */
+public class UseCaseOnePalindromeApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("\nWelcome to Palindrome Checking Management App\nVersion : 1.0\nSystem Initialized successfully\n");
+
+	}
+
+}
